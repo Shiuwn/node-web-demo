246ce7441fa68bedc8e302d371e070820e7e51d0
shiyu.wang <shiyu.wang@ushow.media>
2023-09-01 09:13:55 +0800

    fix: 移除校验代码

--- packages/jwt/index.js
+++ packages/jwt/index.js
@@ -3,12 +3,9 @@ const jwt = require('jsonwebtoken')
 dotenv.config()
 const SECRET = process.env.SECRET
 const token = jwt.sign({ user: 'hello', email: 'hello@test.com' }, SECRET, {
-  expiresIn: '20s'
+  expiresIn: '2s'
 })
 console.log(token)
 const decode = jwt.verify(token, SECRET)
-if (decode.exp < Date.now()) {
-  console.log('valid')
-}
 console.log(decode)
 
